d879ad72cf40883434c659788044590d9fd06381
zfreedman <zfreedman92@gmail.com>
2018-05-10 17:16:58 -0700

    feat: Began implementing first day block

---
+++ src/components/week.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Week extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { index: -1 }
+  }
+
+  render() {
+    return (
+      <div className="block"></div>
+    )
+  }
+}
+
+export default Week;
